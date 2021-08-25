eb2aed64c011731481e14e283d1813769f41b193
Firebat <bbfarmcontact@gmail.com>
2021-08-25 12:25:08 +0900

    Change Addresses

--- src/component/Right.tsx
+++ src/component/Right.tsx
@@ -24,17 +24,17 @@ export default class Right extends Component<{}, {
 
     public async componentDidMount() {
         (async () => {
-            const prices = await PriceCalculatorContract.valueOfAsset("0xA14eBc2cbe4B2f87B0F1aEC4bCa6e410790D8709", await NYCakeContract.getTotalSupply());
+            const prices = await PriceCalculatorContract.valueOfAsset("0x3d6f31679c13b767c7d029efd33fc5f19da40224", await NYCakeContract.getTotalSupply());
             this.setState({ marketCap: prices[1] });
         });
         //TODO: Load Trading VOLUME 24H using https://www.coingecko.com/api/documentations/v3#/coins/get_coins__id__market_chart
 
         // Load LP value.
         (async () => {
-            const wbnb = await WBNBContract.balanceOf("0x3ca164ce12593f856eb9a51d007798d126f5b3fd");
-            const nycake = await NYCakeContract.balanceOf("0x3ca164ce12593f856eb9a51d007798d126f5b3fd");
+            const wbnb = await WBNBContract.balanceOf("0x0e0fdc578548427a9cded233bb4a48c2e20de489");
+            const nycake = await NYCakeContract.balanceOf("0x0e0fdc578548427a9cded233bb4a48c2e20de489");
             const wbnbPrices = await PriceCalculatorContract.valueOfAsset("0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c", wbnb);
-            const nycakePrices = await PriceCalculatorContract.valueOfAsset("0xA14eBc2cbe4B2f87B0F1aEC4bCa6e410790D8709", nycake);
+            const nycakePrices = await PriceCalculatorContract.valueOfAsset("0x3d6f31679c13b767c7d029efd33fc5f19da40224", nycake);
             this.setState({ lpValue: wbnbPrices[1].add(nycakePrices[1]) });
         })();
         (async () => {
